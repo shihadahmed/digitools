7a65e39e2ebe44b42be82a80f5ab4aaf5ac4d93a
shihadahmed <shihadbinahmed@gmail.com>
2026-05-30 01:35:57 +0600

    digitools Landing Page React Project Start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='app-container'>
+      <Navbar/>
+
+    </div>
+
+    
+  )
+}
+
+export default App
